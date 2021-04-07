30f243b408f72efd6c91f71c47d0d5db6038f4be
Ievgen Pervushyn <ievgen@midgardtech.online>
2021-04-07 13:39:39 +0300

    PocketSwap based on UniswapV2

---
+++ contracts/PocketSwap.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity >=0.6.12;
+
+contract PocketSwap {
+
+}
---
+++ contracts/mocks/MockERC20.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity >=0.6.12;
+
+import '../Uniswap/uniswap-v2-core/contracts/interfaces/IERC20.sol';
+import "../Uniswap/uniswap-v2-periphery/contracts/libraries/SafeMath.sol";
+
+contract MockERC20 is IERC20 {
+    using SafeMath for uint;
+
+    string public override constant name = 'ERC20';
+    string public override constant symbol = 'ERC20';
+    uint8 public override constant decimals = 18;
+    uint  public override totalSupply;
+    mapping(address => uint) public override balanceOf;
+    mapping(address => mapping(address => uint)) public override allowance;
+
+    function mint(address to, uint value) external {
+        totalSupply = totalSupply.add(value);
+        balanceOf[to] = balanceOf[to].add(value);
+        emit Transfer(address(0), to, value);
+    }
+
+    function burn(address from, uint value) external {
+        balanceOf[from] = balanceOf[from].sub(value);
+        totalSupply = totalSupply.sub(value);
+        emit Transfer(from, address(0), value);
+    }
+
+    function _approve(address owner, address spender, uint value) private {
+        allowance[owner][spender] = value;
+        emit Approval(owner, spender, value);
+    }
+
+    function _transfer(address from, address to, uint value) private {
+        balanceOf[from] = balanceOf[from].sub(value);
+        balanceOf[to] = balanceOf[to].add(value);
+        emit Transfer(from, to, value);
+    }
+
+    function approve(address spender, uint value) external override returns (bool) {
+        _approve(msg.sender, spender, value);
+        return true;
+    }
+
+    function transfer(address to, uint value) external override returns (bool) {
+        _transfer(msg.sender, to, value);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint value) external override returns (bool) {
+        if (allowance[from][msg.sender] != uint(-1)) {
+            allowance[from][msg.sender] = allowance[from][msg.sender].sub(value);
+        }
+        _transfer(from, to, value);
+        return true;
+    }
+}
